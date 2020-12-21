1de3a4e5a635bf6eb216f28e9b5893c1e507e63d
Fede <chiellofederico@gmail.com>
2020-12-21 16:03:51 +0100

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 // Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-// (1) Stampare i numeri da 1 a 100
+// (1) Stampare i numeri da 1 a 100;
+// (2) I multipli di 3 stampano 'Fizz';
+// (3) I multipli di 5 stampano 'Buzz';
+// (4) I multipli sia di 3 che di 5 stampano 'FizzBuzz'.
 for (var i = 1; i <= 100; i++){
-  if(i % 3 == 0){
+  if(i % 3 == 0 && i % 5 == 0){
+    console.log('FizzBuzz');
+  } else if (i % 3 == 0) {
     console.log('Fizz');
+  } else if (i % 5 == 0){
+    console.log('Buzz');
   } else {
     console.log(i);
   }
